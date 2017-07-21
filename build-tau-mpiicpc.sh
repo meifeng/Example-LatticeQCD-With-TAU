8df48e8b2dc09e45483859dbe16a47e1b1a49622
Meifeng Lin <mflin228@gmail.com>
2017-07-21 15:58:38 -0400

    Added TAU build script

---
+++ build-tau-mpiicpc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+	wget http://tau.uoregon.edu/ext.tgz
+	wget http://tau.uoregon.edu/tau.tgz
+	tar -zxvf tau.tgz
+	tar -zxvf ext.tgz
+	mv external_dependencies tau-2.26.2p1
+	cd tau-2.26.2p1
+	./configure -c++=mpiicpc -cc=mpiicc -mpi -ompt=download -bfd=download -unwind=download 
+	make install
+	
