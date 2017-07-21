323d201223389617ea70f64d8920c96474599907
Meifeng Lin <mflin228@gmail.com>
2017-07-21 16:05:11 -0400

    Updated comments in tau build script

--- build-tau-mpiicpc.sh
+++ build-tau-mpiicpc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Use this script to download and build TAU with Intel compiler and MPI.
+# 
 	wget http://tau.uoregon.edu/ext.tgz
 	wget http://tau.uoregon.edu/tau.tgz
 	tar -zxvf tau.tgz
